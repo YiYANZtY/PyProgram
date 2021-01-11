6085f1a443bad7c0f84c03e11476a46d69d479d9
YiYANZtY <yiyanzty@gmail.com>
2021-01-11 19:20:25 +0800

    add write text function

--- PeppaPig/main.py
+++ PeppaPig/main.py
@@ -278,16 +278,18 @@ def setting():  # 参数设置
 
 def main():
     setting()  # 画布、画笔设置
-    nose(-100, 100)  # 鼻子
-    head(-69, 167)  # 头
-    ears(0, 160)  # 耳朵
-    eyes(0, 140)  # 眼睛
-    cheek(80, 10)  # 腮
-    mouth(-20, 30)  # 嘴
-    body(-32, -8)  # 身体
-    hands(-56, -45)  # 手
-    foot(2, -177)  # 脚
-    tail(148, -155)  # 尾巴
+    # nose(-100, 100)  # 鼻子
+    # head(-69, 167)  # 头
+    # ears(0, 160)  # 耳朵
+    # eyes(0, 140)  # 眼睛
+    # cheek(80, 10)  # 腮
+    # mouth(-20, 30)  # 嘴
+    # body(-32, -8)  # 身体
+    # hands(-56, -45)  # 手
+    # foot(2, -177)  # 脚
+    # tail(148, -155)  # 尾巴
+
+    Turtle().write('so beautiful rose!\n\n')
     done()
 
 
